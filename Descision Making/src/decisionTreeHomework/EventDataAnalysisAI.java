bf01ce89481558cab416fc5f31bcf569e63fa6d4
Michael Hrcek <mhrcek@imsa.edu>
2015-04-15 10:45:27 -0500

    Modified AI structure. Designed helper classes.

---
+++ Descision Making/src/decisionTreeHomework/EventDataAnalysisAI.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decisionTreeHomework;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ *
+ * @author mhrcek
+ */
+public class EventDataAnalysisAI implements Serializable{
+    
+    
+    
+    public void train(Data dataSet, String type){
+        
+    }
+    
+    private class dataRegistry{
+        
+        private Map<String,varRegistry> varRegistries;
+        
+        private dataRegistry(){
+            
+        }
+        
+        private void addDataToRegistry(String registry, var)
+        
+    }
+    
+    private class varRegistry{
+        
+    }
+    
+    private class var{
+        this.mlnub = mlnub;
+		this.transverseMass = transverseMass;
+		this.jet1ET = jet1ET;
+		this.leptonpT = leptonpT;
+		this.hT = hT;
+		this.mET = mET;
+		this.qEta = qEta;
+		this.cosTheta = cosTheta;
+    }
+    
+}
---
+++ Descision Making/src/decisionTreeHomework/EventDataAnalysisAIVarStore.java
@@ -0,0 +1,230 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decisionTreeHomework;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mhrcek
+ */
+public class EventDataAnalysisAIVarStore {
+
+    //Can be made general if using ArrayList<Double>[]
+    ArrayList<Double> mlnub, transverseMass, jet1ET, leptonpT, hT, mET, qEta, cosTheta;
+
+    String associatedType;
+
+    public EventDataAnalysisAIVarStore(String associatedType) {
+        this.associatedType = associatedType;
+        this.mlnub = new ArrayList<>();
+        this.transverseMass = new ArrayList<>();
+        this.jet1ET = new ArrayList<>();
+        this.leptonpT = new ArrayList<>();
+        this.hT = new ArrayList<>();
+        this.mET = new ArrayList<>();
+        this.qEta = new ArrayList<>();
+        this.cosTheta = new ArrayList<>();
+    }
+
+    public String getAssociatedType() {
+        return associatedType;
+    }
+
+    public void addEvent(Homework9Event event) {
+        mlnub.add(event.getVars()[0]);
+        transverseMass.add(event.getVars()[1]);
+        jet1ET.add(event.getVars()[2]);
+        leptonpT.add(event.getVars()[3]);
+        hT.add(event.getVars()[4]);
+        mET.add(event.getVars()[5]);
+        qEta.add(event.getVars()[6]);
+        cosTheta.add(event.getVars()[7]);
+    }
+
+    public double getAverage_mlnub() {
+        double average = 0;
+
+        for (Double value : mlnub) {
+            average += value;
+        }
+
+        return average / (double) mlnub.size();
+    }
+    
+    public double getAverage_transverseMass() {
+        double average = 0;
+
+        for (Double value : transverseMass) {
+            average += value;
+        }
+
+        return average / (double) transverseMass.size();
+    }
+    
+    public double getAverage_jet1ET() {
+        double average = 0;
+
+        for (Double value : jet1ET) {
+            average += value;
+        }
+
+        return average / (double) jet1ET.size();
+    }
+    
+    public double getAverage_leptonpT() {
+        double average = 0;
+
+        for (Double value : leptonpT) {
+            average += value;
+        }
+
+        return average / (double) leptonpT.size();
+    }
+    
+    public double getAverage_hT() {
+        double average = 0;
+
+        for (Double value : hT) {
+            average += value;
+        }
+
+        return average / (double) hT.size();
+    }
+    
+    public double getAverage_mET() {
+        double average = 0;
+
+        for (Double value : mET) {
+            average += value;
+        }
+
+        return average / (double) mET.size();
+    }
+    
+    public double getAverage_qEta() {
+        double average = 0;
+
+        for (Double value : qEta) {
+            average += value;
+        }
+
+        return average / (double) qEta.size();
+    }
+    
+    public double getAverage_cosTheta() {
+        double average = 0;
+
+        for (Double value : cosTheta) {
+            average += value;
+        }
+
+        return average / (double) cosTheta.size();
+    }
+
+    public double[] getRange_mlnub(){
+        double min = mlnub.get(0);
+        double max = mlnub.get(0);
+        
+        for(double value: mlnub){
+            min = Math.min(value, min);
+            max = Math.max(value, max);
+        }
+        
+        double[] range = {min,max};
+        return range;
+        
+    }
+    
+    public double[] getRange_transverseMass(){
+        double min = transverseMass.get(0);
+        double max = transverseMass.get(0);
+        
+        for(double value: transverseMass){
+            min = Math.min(value, min);
+            max = Math.max(value, max);
+        }
+        
+        double[] range = {min,max};
+        return range;
+        
+    }
+    
+    public double[] getRange_jet1ET(){
+        double min = jet1ET.get(0);
+        double max = jet1ET.get(0);
+        
+        for(double value: jet1ET){
+            min = Math.min(value, min);
+            max = Math.max(value, max);
+        }
+        
+        double[] range = {min,max};
+        return range;
+        
+    }
+    
+    public double[] getRange_leptonpT(){
+        double min = leptonpT.get(0);
+        double max = leptonpT.get(0);
+        
+        for(double value: leptonpT){
+            min = Math.min(value, min);
+            max = Math.max(value, max);
+        }
+        
+        double[] range = {min,max};
+        return range;
+        
+    }
+    
+    public double[] getRange_mlnub(){
+        double min = mlnub.get(0);
+        double max = mlnub.get(0);
+        
+        for(double value: mlnub){
+            min = Math.min(value, min);
+            max = Math.max(value, max);
+        }
+        
+        double[] range = {min,max};
+        return range;
+        
+    }
+    
+    public double[] getRange_mlnub(){
+        double min = mlnub.get(0);
+        double max = mlnub.get(0);
+        
+        for(double value: mlnub){
+            min = Math.min(value, min);
+            max = Math.max(value, max);
+        }
+        
+        double[] range = {min,max};
+        return range;
+        
+    }
+    
+    public double[] getRange_mlnub(){
+        double min = mlnub.get(0);
+        double max = mlnub.get(0);
+        
+        for(double value: mlnub){
+            min = Math.min(value, min);
+            max = Math.max(value, max);
+        }
+        
+        double[] range = {min,max};
+        return range;
+        
+    }
+    
+    public double][] getProbableRange_mlnub(){
+        
+    }
+    
+}
